280f97bbe93fe52a591ca2f32b6fc1feed00e66a
Elyas Ozbek <109240976+ElyasO03@users.noreply.github.com>
2024-01-16 18:09:34 -0500

    namespace change

--- C Sharp Class II/Program.cs
+++ C Sharp Class II/Program.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace C_Sharp_Class_II
+namespace ITP_236
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
