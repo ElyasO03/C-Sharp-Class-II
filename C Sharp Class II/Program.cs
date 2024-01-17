6008dd428f57fddac9463ea45c81a232fccd9024
Elyas Ozbek <109240976+ElyasO03@users.noreply.github.com>
2024-01-16 20:50:22 -0500

    Week 1

--- C Sharp Class II/Program.cs
+++ C Sharp Class II/Program.cs
@@ -2,14 +2,81 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using static System.Console;
 
-namespace ITP_236
+namespace Week1_Class
 {
-    class Program
+    internal class Program
     {
+        static List<decimal> amounts = new List<decimal>();
+        static List<string> categories = new List<string>();
+        static List<DateTime> dates = new List<DateTime>();
         static void Main(string[] args)
         {
 
+            WriteLine("1. Add Expense");
+            WriteLine("2. View Summary");
+            WriteLine("3. Search Expenses");
+            WriteLine("4. Exit");
+            Write("Which Option would like: ");
+
+            switch(ReadLine())
+            {
+                case "1": 
+                    // method call passing 3 arguments
+                    AddExpense(); 
+                    break;
+                    case "2":
+                        break;
+                    case "3":
+                    SearchExpenses();
+                        break;
+
+                    default:
+                    WriteLine("Invalid Option!");
+                    break;
+            }// end switch
+
+            ReadKey();
+        }// end main
+
+        static void AddExpense()
+        {
+            Write("Enter amount: ");
+            if (decimal.TryParse(ReadLine(), out decimal amount))
+            {
+                Write("Please enter category: ");
+                string category = ReadLine();
+
+                Write("Enter date yyyy-mm-dd");
+                if (DateTime.TryParse(ReadLine(), out DateTime date))
+                {
+                    amounts.Add(amount);
+                    categories.Add(category);
+                    dates.Add(date);
+                    WriteLine("Expenses added");
+                }
+                else
+                    WriteLine("Invalid Date");
+            }// end if
+            else
+                WriteLine("Invalid Amount");
+        }
+
+        static void SearchExpenses() 
+        {
+            WriteLine("Enter a category to search: ");
+            string searchCat = ReadLine();
+
+            WriteLine("Expenses in the category Entered");
+
+            foreach(var c in categories)
+            {
+                if(c.Equals(searchCat, StringComparison.OrdinalIgnoreCase))
+                {
+                    WriteLine($"Expense cat: {c}");
+                }
+            }
         }
     }
 }
