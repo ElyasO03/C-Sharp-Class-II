a052b2c27031d3cd7cfb130fe75c59fb1c6f4c92
Elyas Ozbek <109240976+ElyasO03@users.noreply.github.com>
2024-01-31 15:01:55 -0500

    Week2

---
+++ Week2_Class/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static Week2_Class.Shapes;
+
+namespace Week2_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Week2_Class.Shapes.Rectangle(20, 25);
+
+            ShowShapes(r1);
+
+            var s1 = new Square(5);
+            ShowShapes(s1);
+
+            var s2 = new Square();
+            ShowShapes(s2);
+
+            Circle c1 = new Circle(10);
+            ShowShapes(c1);
+
+            ReadKey();
+        }//ends main
+
+        static void ShowShapes(Shapes s)
+        {
+            WriteLine($"{s.GetType().Name}\t\tArea = {s.Area}\t\tPerimeter = {s.Perimeter}\t\tUOM= {s.UoM}");
+            WriteLine();
+        }
+
+    }
+}
---
+++ Week2_Class/Shapes.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Class
+{
+    public abstract class Shapes
+    {
+
+        //Constructor
+
+
+        public Shapes()
+        {
+            UoM = "inches";
+        }
+
+        public string UoM { get; set; }
+        
+        public abstract double Area { get; }
+
+        public abstract double Perimeter { get; }
+
+        //ends shape
+
+        public class Rectangle : Shapes
+        {
+            public Rectangle(double l, double w) : base()
+            {
+                Length = l;
+                Width = w;
+            }
+            //default constructor...this is passing the Rectangle (l,w)
+
+            public Rectangle():this(1,1)
+            {
+
+            }
+
+            public double Length { get; set; }
+
+            public double Width { get; set; }
+
+            public override double Area
+            {
+                get { return Length * Width; }
+            }
+
+            public override double Perimeter
+            {
+                get { return 2 * (Length + Width); }
+            }
+        }
+        public class Square : Shapes
+        {
+            public Square(double l): base()
+            {
+                Length = l;
+            }
+            public Square() : this(1) { }
+
+            public override double Area { get { return Length * Length; } }
+
+            public override double Perimeter { get { return Length * 4;} }
+            //area is length times length
+            //perimeter is lengths times 4
+
+            public double Length { get; set; }
+        }
+    }
+
+    public class Circle : Shapes
+    {
+        public Circle(double r): base()
+        {
+            Radius = r;
+        }
+
+        public Circle(): this (1)
+        {
+
+        }
+
+        public double Radius { get; set; }
+
+        public override double Area
+        {
+            get { return Math.PI * Radius * Radius; }
+        }
+
+        public override double Perimeter
+        {
+            get { return 2 * Math.PI * Radius; }
+        }
+    }
+}
